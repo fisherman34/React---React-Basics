d0e6a0056da3a238e92a0c90958a4d433bb944c3
Sam Xu <fisherman34@qq.com>
2026-05-14 23:38:59 +0900

    add exer18

---
+++ 18. Custom Components - Parent Child Components/src/App.jsx
@@ -0,0 +1,53 @@
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+/** Mini Challenge:
+ * 
+ * Move the `header` element from the Page component into 
+ * its own component called "Header"
+ * 
+ * Then render an instance of the Header component inside
+ * the Page component where the `header` used to be.
+ */
+
+function Header() {
+  return (
+    <header>
+      <img src="/src/assets/react-logo.png" width="40px" alt="React logo" />
+  </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+        <h1>Reason I am excited to learn React</h1>
+        <ol>
+            <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+            <li>I am more likely to get a job as a front end developer if I know React</li>
+        </ol>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+        <small>© 2024 Ziroll development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function Page() {
+    return (
+        <>
+          <Header />
+          <MainContent />
+          <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
