4941c7ebe943dd422c769118453e12767af393da
Sam Xu <fisherman34@qq.com>
2026-05-17 22:10:46 +0900

    add exer09

--- 02. Data-Driven React/08. Props Part 3 - Create a contact component/src/App.jsx
+++ 02. Data-Driven React/08. Props Part 3 - Create a contact component/src/App.jsx
@@ -11,10 +11,30 @@ import Contact from "./Contact"
 function App() {
     return (
         <div className="contacts">
-            <Contact />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact 
+                img="./images/mr-whiskerson.png"
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskers@catmail.com"
+            />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
         </div>
     )
 }
