8967985cdaad2373ac5d9fa1772d3f4b151bb907
Sam Xu <fisherman34@qq.com>
2026-05-19 22:41:03 +0900

    add exer21

--- 02. Data-Driven React/19. Mapping components/src/App.jsx
+++ 02. Data-Driven React/19. Mapping components/src/App.jsx
@@ -2,7 +2,6 @@ import Joke from "./Joke"
 import jokesData from "./jokesData"
 
 export default function App() {
-    
     const jokeElements = jokesData.map((joke) => {
         return <Joke  setup={joke.setup} punchline={joke.punchline} />
     })
