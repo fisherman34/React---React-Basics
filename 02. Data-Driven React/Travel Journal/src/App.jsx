7e289c560692728b0f5eebb1e7273f518ce8f5c1
Sam Xu <fisherman34@qq.com>
2026-05-21 00:03:54 +0900

    add exer24

--- 02. Data-Driven React/Travel Journal/src/App.jsx
+++ 02. Data-Driven React/Travel Journal/src/App.jsx
@@ -19,7 +19,8 @@ export default function App() {
         return (
             <Entry 
                 key={entry.id}
-                entry={entry}
+                {...entry}
+                 
             />
         )
     })
