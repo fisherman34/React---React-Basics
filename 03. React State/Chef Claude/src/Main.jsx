354a4554954b60d7bb7445ed90ae89678f9e4f37
Sam Xu <fisherman34@qq.com>
2026-05-26 00:08:08 +0900

    add exer25

--- 03. React State/Chef Claude/src/Main.jsx
+++ 03. React State/Chef Claude/src/Main.jsx
@@ -7,26 +7,14 @@ export default function Main() {
       <li key={ingredient}>{ingredient}</li>
     ))
     
-    /**
-     * function handleSubmit(event) の event は、「イベント情報」が入ったオブジェクトです。
-     * フォームが submit（送信）された時に handleSubmit を実行する。
-     * その時 React は、自動的に submit に関する情報をeventへ渡します
-     */
-    function handleSubmit(event) {
-      // event.preventDefault() フォーム送信の通常動作をキャンセルする
-      event.preventDefault()
-      // event.currentTargetはイベントが発生した対象要素、今回は submit イベントなので、
-      // <form> ... </form>を指しています
-      // FormData はブラウザ標準の機能で、フォームの入力データをまとめて取得するためのオブジェクト
-      const formData = new FormData(event.currentTarget)
-      // .get()は、ingredient という名前の入力値を取得
+    function addIngredient(formData) {
       const newIngredient = formData.get("ingredient")
       setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
