88005716ea78995cdb663e431eb772d7dbdf7dee
Sam Xu <fisherman34@qq.com>
2026-05-26 22:56:18 +0900

    add exer32

---
+++ 03. React State/32. Conditional rendering/src/App.jsx
@@ -0,0 +1,19 @@
+import Joke from "/src/Joke"
+import jokesData from "/src/jokesData"
+
+export default function App() {
+    const jokeElements = jokesData.map(joke => {
+        return (
+            <Joke 
+                key={joke.id}
+                setup={joke.setup} 
+                punchline={joke.punchline} 
+            />
+        )
+    })
+    return (
+        <div>
+            {jokeElements}
+        </div>
+    )
+}
---
+++ 03. React State/32. Conditional rendering/src/Joke.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Joke(props) {
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
+    const [isShown, setIsShown] = React.useState(false)
+
+    // prevShown means the previous value of the state variable isShown
+    function toggleIsShown() {
+      setIsShown(prevShown => !prevShown)
+    }
+    
+    console.log(isShown)
+
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleIsShown}>Show punchline</button>
+            <hr />
+        </div>
+    )
+}
