932d456c0bc52cf16bbcb23a98f7589c933e85ae
Sam Xu <fisherman34@qq.com>
2026-05-27 23:58:41 +0900

    add exer38

--- 03. React State/Chef Claude/src/Main.jsx
+++ 03. React State/Chef Claude/src/Main.jsx
@@ -1,21 +1,28 @@
 import React from "react"
 
 export default function Main() {
+
     const [ingredients, setIngredients] = React.useState([])
 
-    const ingredientsList = ingredients.map(ingredient => (
-      <li key={ingredient}>{ingredient}</li>
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
     ))
-    
+
     function addIngredient(formData) {
-      const newIngredient = formData.get("ingredient")
-      setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
+    
+    /**
+     * Challenge:
+     * Using conditional rendering, only render the new <section> IF
+     * there are ingredients added to the list of ingredients.
+     */
 
     return (
         <main>
             <form action={addIngredient} className="add-ingredient-form">
-                <input 
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
@@ -23,9 +30,17 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul> 
-                {ingredientsList}
-            </ul>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
         </main>
     )
-}
+} 
