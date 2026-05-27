c014b8ffa9838456efecdfd0a53cd60c17175d0a
Sam Xu <fisherman34@qq.com>
2026-05-27 22:39:32 +0900

    add exer35

--- 03. React State/32. Conditional rendering/src/Joke.jsx
+++ 03. React State/32. Conditional rendering/src/Joke.jsx
@@ -18,8 +18,9 @@ export default function Joke(props) {
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            {isShown && <p>{props.punchline}</p>}
-            <button onClick={toggleIsShown}>Show punchline</button>
+            {isShown ? <p>{props.punchline}</p> : null}
+            <button onClick={toggleIsShown}>{isShown ? "Hide" : "Show"} punchline</button>
+        
             <hr />
         </div>
     )
