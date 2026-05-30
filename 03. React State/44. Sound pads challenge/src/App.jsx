c9fa08e3bc7a9e25c8eb1454f8a67c519b341bd0
Sam Xu <fisherman34@qq.com>
2026-05-30 16:29:36 +0900

    add exer45

---
+++ 03. React State/44. Sound pads challenge/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import padsData from "./pads"
+
+export default function App() {
+    const [pads, setPads] = React.useState(padsData)
+
+
+    const buttonElements = pads.map(pad => (
+        <button key={pad.id}></button>
+    ))
+
+    return (
+        <main>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
+        </main>
+    )
+}
