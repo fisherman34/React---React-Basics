17903872fa17b40a07427e04f1b0cca7322e6da5
Sam Xu <fisherman34@qq.com>
2026-05-30 21:12:35 +0900

    add exer46

--- 03. React State/44. Sound pads challenge/src/App.jsx
+++ 03. React State/44. Sound pads challenge/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react"
 import padsData from "./pads"
+import Pad from "./Pad"
 
-export default function App() {
+export default function App({ darkMode }) {
     const [pads, setPads] = React.useState(padsData)
 
-
+    const styles = {
+        backgroundColor: darkMode ? "#222222" : "#cccccc"
+    }
+    /**
+     * key={pad.id} は、React がリストで生成された各要素を識別するための特別な属性です。
+     * pads.map() で配列 pads の各データから複数の <button> を作っています
+     * React はこういうとき、「どの button がどのデータに対応しているのか」を区別する必要があります。
+     * そのために key を使います。
+     * 
+     */
     const buttonElements = pads.map(pad => (
-        <button key={pad.id}></button>
+        <Pad key={pad.id} color={pad.color} />
     ))
 
     return (
