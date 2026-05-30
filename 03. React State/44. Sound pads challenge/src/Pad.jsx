9ddfc8068c3d401aa7a65a12aa52ea8475dbb928
Sam Xu <fisherman34@qq.com>
2026-05-30 21:29:44 +0900

    add exer47

--- 03. React State/44. Sound pads challenge/src/Pad.jsx
+++ 03. React State/44. Sound pads challenge/src/Pad.jsx
@@ -1,8 +1,16 @@
+import React from "react"
+
 export default function Pad(props) {
+  const [on, setOn] = React.useState(props.on)
+
+  function toggle() {
+    setOn(prevOn => !prevOn)
+  }
   return (
     <button
       style={{backgroundColor: props.color}}
-      className={props.on ? "on" : undefined}
+      className={on ? "on" : undefined}
+      onClick={toggle}
     ></button>
   )
 }
