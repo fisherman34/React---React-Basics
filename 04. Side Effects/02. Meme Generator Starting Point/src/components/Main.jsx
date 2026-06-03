f9bc7ffde0b5f0d9f515cb5d93bf60b017c07b87
Sam Xu <fisherman34@qq.com>
2026-06-03 22:13:52 +0900

    add exer05

--- 04. Side Effects/02. Meme Generator Starting Point/src/components/Main.jsx
+++ 04. Side Effects/02. Meme Generator Starting Point/src/components/Main.jsx
@@ -8,10 +8,10 @@ export default function Main() {
     })
 
     function handleChange(event) {
-        const {value} = event.currentTarget
+        const {value, name} = event.currentTarget
         setMeme(prevMeme => ({
             ...prevMeme,
-            topText: value,
+            [name]: value,
         }))
     }
     return (
@@ -23,6 +23,7 @@ export default function Main() {
                         placeholder="One does not simply"
                         name="topText"
                         onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -31,6 +32,8 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
